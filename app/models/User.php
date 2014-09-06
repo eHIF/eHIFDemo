f276c4aa1ed779ce1d2a263a98e4929bbce1b84f
ʌἁɀɑƿѻѕ <larjohn@gmail.com>
2014-09-06 19:56:38 +0300

    Communication with Activiti now possible

---
+++ app/models/User.php
@@ -0,0 +1,12 @@
+<?php
+
+use Zizaco\Confide\ConfideUser;
+use Zizaco\Confide\ConfideUserInterface;
+use Zizaco\Entrust\HasRole;
+
+class User extends Eloquent implements ConfideUserInterface
+{
+    use ConfideUser;
+    use HasRole; // Add this trait to your user model
+
+}
